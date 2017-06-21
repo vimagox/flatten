1a923a75b6165dd7046327962b6f71e7d3374e10
Pancho Rifa <victor@1910.io>
2017-06-21 11:58:50 -0600

    Adding docs to flatten function

--- src/flatten.js
+++ src/flatten.js
@@ -1,3 +1,9 @@
+/**
+ * Flattens/Merges an array of arrays.
+ * Returns duplicate values.
+ * @param {Array} array
+ * @return {Array} flatten_array
+ */
 exports.flatten = function(array) {
   return [].concat.apply([], array)
 }
